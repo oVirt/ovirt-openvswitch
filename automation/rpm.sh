88fd54bd8e9673a9875664eea958d69864b09eca
Ales Musil <amusil@redhat.com>
2021-12-03 08:47:50 +0100

    Introduce Github CI
    Add CI job that will build and upload artifacts.
    
    Signed-off-by: Ales Musil <amusil@redhat.com>

---
+++ automation/rpm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xe
+
+EXPORT_DIR="${EXPORT_DIR:=exported-artifacts}"
+
+mkdir -p $EXPORT_DIR
+
+rpmbuild --define "%_topdir `pwd`" -ba SPEC/ovirt-openvswitch.spec
+
+cp RPMS/noarch/ovirt-openvswitch-*.noarch.rpm $EXPORT_DIR/
+cp RPMS/noarch/ovirt-python-openvswitch-*.noarch.rpm $EXPORT_DIR/
+cp SRPMS/ovirt-openvswitch-*.src.rpm $EXPORT_DIR/
+
+createrepo_c "$EXPORT_DIR"
